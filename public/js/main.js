e74047c3dec46f4b823c9d035315480e943d5a44
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-21 10:07:44 +0100

    boucle while fini - 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,15 @@
-for (let index = 1; index < 11; index++) {
-    console.log(index);
+// BOUCLE #1
+
+// for (let index = 1; index < 11; index++) {
+//     console.log(index);
     
+// }
+
+
+// BOUCLE #2
+
+let n = 11; 
+while (n > 1) {
+    n--
+    console.log(n);
 }
